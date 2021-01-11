d5d07cd87aefff2913120d4fece928997a1330d2
crane0 <bhad5683126@163.com>
2021-01-11 23:43:45 +0800

    feat: 第2遍 38-40

--- 第2遍/039从前序与中序遍历序列构造二叉树.js
+++ 第2遍/039从前序与中序遍历序列构造二叉树.js
@@ -15,5 +15,23 @@
  * @return {TreeNode}
  */
 var buildTree = function(preorder, inorder) {
-  
+  let map = new Map()
+  for (let i = 0; i < inorder.length; i++) {
+    map.set(inorder[i], i)
+  }
+  return build(preorder, 0, preorder.length, inorder, 0, inorder.length)
+
+  function build(preorder, p_start, p_end, inorder, i_start, i_end) {
+    // 上次循环进入后，如果子树长度为0，那 p_start === p_end
+    if (p_start === p_end) return null;
+    const rootVal = preorder[p_start]
+    const node = new TreeNode(rootVal)
+    const rootIndex = map.get(rootVal)
+    // 这是根据 inorder 得出的左子树长度！！！，所以才可以在 preorder 中确定左子树的长度！
+    const leftLength = rootIndex - i_start
+
+    node.left = build(preorder, p_start + 1, p_start + 1 + leftLength, inorder, i_start, rootIndex)
+    node.right = build(preorder, p_start + 1 + leftLength, p_end, inorder, rootIndex + 1, i_end)
+    return node
+  }
 };
--- 第2遍/040组合.js
+++ 第2遍/040组合.js
@@ -8,7 +8,26 @@
  * @return {number[][]}
  */
 var combine = function(n, k) {
+  if (n < k || k <= 0) return []
+  let queue = [], ret = []
+  dfs(n, k, 1, queue)
+  return ret
 
+  function dfs(n, k, begin, queue) {
+    if (k === 0) {
+      ret.push([...queue])
+      return
+    }
+    // 剪枝上界
+    if (begin > n - k + 1) {
+      return
+    }
+    dfs(n, k, begin + 1, queue)
+
+    queue.push(begin)
+    dfs(n, k-1, begin + 1, queue)
+    queue.pop(begin)
+  }
 };
 
 /* 
