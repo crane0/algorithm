29c34a619170af0cc66f40b2cbdfac5f867b8033
crane0 <bhad5683126@163.com>
2021-03-18 10:54:45 +0800

    feat: 第2遍,67-68

--- 第2遍/067不同路径.js
+++ 第2遍/067不同路径.js
@@ -7,7 +7,25 @@
  * @return {number}
  */
 var uniquePaths = function(m, n) {
-  
+  if (m === 1) return 1
+  let arr = []
+  for (let i = 0; i < m; i++) {
+    arr[i] = []
+    for (let j = 0; j < n; j++) {
+      if (i === 0 || j === 0) {
+        arr[i][j] = 1
+      } else {
+        arr[i][j] = 0
+      }
+    }
+  }
+
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      arr[i][j] = arr[i-1][j] + arr[i][j-1]
+    }
+  }
+  return arr[m-1][n-1]
 }
 
 
@@ -15,6 +33,14 @@ var uniquePaths = function(m, n) {
   空间复杂度从 O(m*n) 优化到 O(n)
 */
 var uniquePaths = function(m, n) {
-  
+  if (m === 1) return 1
+  let arr = new Array(n).fill(1)
+
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      arr[j] += arr[j-1]
+    }
+  }
+  return arr[n-1]
 }
 console.log(uniquePaths(3, 7))
--- 第2遍/068不同路径II.js
+++ 第2遍/068不同路径II.js
@@ -7,6 +7,24 @@
  * @return {number}
  */
 var uniquePathsWithObstacles = function(obstacleGrid) {
+  if (obstacleGrid[0][0] === 1) return 0;
+
+  const m = obstacleGrid.length, n = obstacleGrid[0].length
+  let arr = new Array(n).fill(0)
+  arr[0] = 1
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (obstacleGrid[i][j] === 1) {
+        arr[j] = 0
+        continue
+      }
+      if (j > 0) {
+        arr[j] += arr[j-1]
+      }
+    }
+  }
+  return arr[n-1]
   
 }
 console.log(uniquePathsWithObstacles([[0,0,0],[0,1,0],[0,0,0]]))
